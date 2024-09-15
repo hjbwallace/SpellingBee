62e6c311aade5ae289ce1e8c11562147caff4b37
Harrison Wallace <hjbwallace@gmail.com>
2024-09-15 22:30:33 +0800

    Refactoring speaking logic to AudioEngine class

--- spellingbee.js
+++ spellingbee.js
@@ -9,15 +9,7 @@ function initialise() {
 }
 
 function speak() {
-  // Create a SpeechSynthesisUtterance
-  const utterance = new SpeechSynthesisUtterance("Hello, world!");
-
-  // Select a voice
-  const voices = speechSynthesis.getVoices();
-  utterance.voice = voices[0];
-
-  // Speak the text
-  speechSynthesis.speak(utterance);
+  AudioEngine.speak("Hello static world!");
 }
 
 function onFormSubmitted(e) {
@@ -38,3 +30,17 @@ function onFormSubmitted(e) {
 }
 
 window.addEventListener('load', initialise);
+
+class AudioEngine {
+  static speak(text) {
+    // Create a SpeechSynthesisUtterance
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // Select a voice
+  const voices = speechSynthesis.getVoices();
+  utterance.voice = voices[0];
+
+  // Speak the text
+  speechSynthesis.speak(utterance);
+  }
+}
